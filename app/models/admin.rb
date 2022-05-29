3d2168ffd4182d3f39553669db84b2b12217a7c1
Vladislav Morgunov <Zoncovsky@icloud.com>
2022-05-29 19:23:51 +0300

    add confirm to admin and seller

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   validates :name, :email, :password, presence: true
   validates :email, :password, length: { minimum: 4 }
